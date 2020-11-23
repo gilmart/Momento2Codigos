3fb474f2e812997d625e2af85131e8215cec1fb8
gilmart <71158983+gilmart@users.noreply.github.com>
2020-11-22 22:17:06 -0500

    Update editarProductos.php

--- editarProductos.php
+++ editarProductos.php
@@ -7,7 +7,8 @@ if(isset($_POST["botonEditar"])){
 
     $nombre=$_POST["nombreEditar"];
     $descripcion=$_POST["descripcionEditar"];
-
+    $precio=$_POST["precioEditar"];
+   
 $transaccion = new baseDatosProductos();
 $consultaSQL="UPDATE productos SET nombre='$nombre', precio='$precio', descripcion='$descripcion' WHERE idProducto='$id'";
 $transaccion->editarDatos($consultaSQL);
